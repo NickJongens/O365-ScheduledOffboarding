5143edf8b55c20ff788db42b4446fcf99f32fce2
Nick Jongens <nick@jongens.nz>
2022-01-19 22:45:24 +1300

    General Codebase Update

---
+++ AzureFunction/Create/OffboardUserAction.ps1
@@ -0,0 +1,65 @@
+$ErrorActionPreference = ‘SilentlyContinue’
+write-host "Logging in to M365 using the secure application model" -ForegroundColor Green
+$credential = New-Object System.Management.Automation.PSCredential($ApplicationId, $ApplicationSecret)
+$aadGraphToken = New-PartnerAccessToken -ApplicationId $ApplicationId -Credential $credential -RefreshToken $refreshToken -Scopes 'https://graph.windows.net/.default' -ServicePrincipal -Tenant $CustomerDefaultDomainname
+$graphToken = New-PartnerAccessToken -ApplicationId $ApplicationId -Credential $credential -RefreshToken $refreshToken -Scopes 'https://graph.microsoft.com/.default' -ServicePrincipal -Tenant $CustomerDefaultDomainname
+write-host "Logging into Azure AD." -ForegroundColor Green
+Connect-AzureAD -AadAccessToken $aadGraphToken.AccessToken -AccountId $UPN -MsAccessToken $graphToken.AccessToken -TenantId $CustomerDefaultDomainname
+write-host "Connecting to Exchange Online" -ForegroundColor Green
+$token = New-PartnerAccessToken -ApplicationId 'a0c73c16-a7e3-4564-9a95-2bdf47383716'-RefreshToken $ExchangeRefreshToken -Scopes 'https://outlook.office365.com/.default' -Tenant $CustomerDefaultDomainname
+$tokenValue = ConvertTo-SecureString "Bearer $($token.AccessToken)" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($upn, $tokenValue)
+$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://ps.outlook.com/powershell-liveid?DelegatedOrg=$($CustomerDefaultDomainname)&BasicAuthToOAuthConversion=true" -Credential $credential -Authentication Basic -AllowRedirection
+Import-PSSession $session -AllowClobber
+write-host "Removing users from Azure AD Groups" -ForegroundColor Green
+
+Try
+{
+$MemberID = (Get-AzureADUser -ObjectId $userToOffboard).objectId
+Get-AzureADUserMembership -ObjectId $MemberID -All $true | Where-Object { $_.ObjectType -eq "Group" -and $_.SecurityEnabled -eq $true -and $_.MailEnabled -eq $false } | where {($_.displayname -notlike "All Users")} | ForEach-Object { 
+    write-host "    Removing user from the group: $($_.displayname)" -ForegroundColor green
+    Remove-AzureADGroupMember -ObjectId $_.ObjectID -MemberId $MemberID -ErrorAction SilentlyContinue
+}
+}
+catch {
+    Write-Warning "Ignoring Error due to Dynamic Group Membership "
+}
+
+write-host "Removing users from Unified Groups and Teams" -ForegroundColor Green
+$OffboardingDN = (get-mailbox -Identity $userToOffboard -IncludeInactiveMailbox).DistinguishedName
+
+
+Get-Recipient -Filter "Members -eq '$OffboardingDN'" -RecipientTypeDetails 'GroupMailbox' | foreach-object { 
+    write-host "    Removing using from $($_.name)" -ForegroundColor green
+    Remove-UnifiedGroupLinks -Identity $_.ExternalDirectoryObjectId -Links $userToOffboard -LinkType Member -Confirm:$false }
+
+write-host "Removing users from Distribution Groups" -ForegroundColor Green
+Get-Recipient -Filter "Members -eq '$OffboardingDN'" | foreach-object { 
+    write-host "    Removing using from $($_.name)" -ForegroundColor green
+    Remove-DistributionGroupMember -Identity $_.ExternalDirectoryObjectId -Member $OffboardingDN -BypassSecurityGroupManagerCheck -Confirm:$false }
+
+write-host "Changing Display Name to Include 'ARCHIVED - ' " -ForegroundColor Green
+$str1 = "ARCHIVED - "
+$str2 = Get-AzureADUser -ObjectId $MemberID | select -expand DisplayName
+$newuserDisplayName = $str1+$str2
+Set-AzureADUser -ObjectId $MemberID -DisplayName $newuserDisplayName
+write-host "Setting mailbox to Shared Mailbox" -ForegroundColor Green
+Set-Mailbox $userToOffboard -Type Shared
+write-host "Hiding user from GAL" -ForegroundColor Green
+Set-Mailbox $userToOffboard -HiddenFromAddressListsEnabled $true
+ 
+write-host "Removing License from user." -ForegroundColor Green
+$AssignedLicensesTable = Get-AzureADUser -ObjectId $userToOffboard | Get-AzureADUserLicenseDetail | Select-Object @{n = "License"; e = { $_.SkuPartNumber } }, skuid 
+if ($AssignedLicensesTable) {
+    $body = @{
+        addLicenses    = @()
+        removeLicenses = @($AssignedLicensesTable.skuid)
+    }
+    Set-AzureADUserLicense -ObjectId $userToOffboard -AssignedLicenses $body
+}
+ 
+write-host "Removed licenses:"
+$AssignedLicensesTable
+write-host "Disabling User sign in" -ForegroundColor Green
+Set-AzureADUser -ObjectID $userToOffboard -AccountEnabled $false
+Remove-PSSession $session
--- AzureFunction/Create/run.ps1
+++ AzureFunction/Create/run.ps1
@@ -12,26 +12,88 @@ else {
   $Logo = ""
 }
 
-$Username = $Request.Query.username
-if (!$Username){
-$Username = ([System.Web.HttpUtility]::ParseQueryString($Request.Body))['username']
+$userToOffboard = $Request.Query.username
+if (!$userToOffboard){
+$userToOffboard = ([System.Web.HttpUtility]::ParseQueryString($Request.Body))['username']
 }
 
-if ($Username) {
-  import-module .\PSfunctions.psm1
-  import-module MSOnline -UseWindowsPowerShell
-  $MFARequest = New-MFARequest -EmailToPush $Username
+if ($userToOffboard) {
+  $ErrorActionPreference = ‘SilentlyContinue’
+  write-host "Logging in to M365 using the secure application model" -ForegroundColor Green
+  $credential = New-Object System.Management.Automation.PSCredential($ApplicationId, $ApplicationSecret)
+  $aadGraphToken = New-PartnerAccessToken -ApplicationId $ApplicationId -Credential $credential -RefreshToken $refreshToken -Scopes 'https://graph.windows.net/.default' -ServicePrincipal -Tenant $CustomerDefaultDomainname
+  $graphToken = New-PartnerAccessToken -ApplicationId $ApplicationId -Credential $credential -RefreshToken $refreshToken -Scopes 'https://graph.microsoft.com/.default' -ServicePrincipal -Tenant $CustomerDefaultDomainname
+  write-host "Logging into Azure AD." -ForegroundColor Green
+  Connect-AzureAD -AadAccessToken $aadGraphToken.AccessToken -AccountId $UPN -MsAccessToken $graphToken.AccessToken -TenantId $CustomerDefaultDomainname
+  write-host "Connecting to Exchange Online" -ForegroundColor Green
+  $token = New-PartnerAccessToken -ApplicationId 'a0c73c16-a7e3-4564-9a95-2bdf47383716'-RefreshToken $ExchangeRefreshToken -Scopes 'https://outlook.office365.com/.default' -Tenant $CustomerDefaultDomainname
+  $tokenValue = ConvertTo-SecureString "Bearer $($token.AccessToken)" -AsPlainText -Force
+  $credential = New-Object System.Management.Automation.PSCredential($upn, $tokenValue)
+  $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://ps.outlook.com/powershell-liveid?DelegatedOrg=$($CustomerDefaultDomainname)&BasicAuthToOAuthConversion=true" -Credential $credential -Authentication Basic -AllowRedirection
+  Import-PSSession $session -AllowClobber
+  write-host "Removing users from Azure AD Groups" -ForegroundColor Green
+  
+  Try
+  {
+  $MemberID = (Get-AzureADUser -ObjectId $userToOffboard).objectId
+  Get-AzureADUserMembership -ObjectId $MemberID -All $true | Where-Object { $_.ObjectType -eq "Group" -and $_.SecurityEnabled -eq $true -and $_.MailEnabled -eq $false } | where {($_.displayname -notlike "All Users")} | ForEach-Object { 
+      write-host "    Removing user from the group: $($_.displayname)" -ForegroundColor green
+      Remove-AzureADGroupMember -ObjectId $_.ObjectID -MemberId $MemberID -ErrorAction SilentlyContinue
+  }
+  }
+  catch {
+      Write-Warning "Ignoring Error due to Dynamic Group Membership "
+  }
+  
+  write-host "Removing users from Unified Groups and Teams" -ForegroundColor Green
+  $OffboardingDN = (get-mailbox -Identity $userToOffboard -IncludeInactiveMailbox).DistinguishedName
+   
+   
+  Get-Recipient -Filter "Members -eq '$OffboardingDN'" -RecipientTypeDetails 'GroupMailbox' | foreach-object { 
+      write-host "    Removing using from $($_.name)" -ForegroundColor green
+      Remove-UnifiedGroupLinks -Identity $_.ExternalDirectoryObjectId -Links $userToOffboard -LinkType Member -Confirm:$false }
+   
+  write-host "Removing users from Distribution Groups" -ForegroundColor Green
+  Get-Recipient -Filter "Members -eq '$OffboardingDN'" | foreach-object { 
+      write-host "    Removing using from $($_.name)" -ForegroundColor green
+      Remove-DistributionGroupMember -Identity $_.ExternalDirectoryObjectId -Member $OffboardingDN -BypassSecurityGroupManagerCheck -Confirm:$false }
+    
+  write-host "Changing Display Name to Include 'ARCHIVED - ' " -ForegroundColor Green
+  $str1 = "ARCHIVED - "
+  $str2 = Get-AzureADUser -ObjectId $MemberID | select -expand DisplayName
+  $newuserDisplayName = $str1+$str2
+  Set-AzureADUser -ObjectId $MemberID -DisplayName $newuserDisplayName
+  write-host "Setting mailbox to Shared Mailbox" -ForegroundColor Green
+  Set-Mailbox $userToOffboard -Type Shared
+  write-host "Hiding user from GAL" -ForegroundColor Green
+  Set-Mailbox $userToOffboard -HiddenFromAddressListsEnabled $true
+   
+  write-host "Removing License from user." -ForegroundColor Green
+  $AssignedLicensesTable = Get-AzureADUser -ObjectId $userToOffboard | Get-AzureADUserLicenseDetail | Select-Object @{n = "License"; e = { $_.SkuPartNumber } }, skuid 
+  if ($AssignedLicensesTable) {
+      $body = @{
+          addLicenses    = @()
+          removeLicenses = @($AssignedLicensesTable.skuid)
+      }
+      Set-AzureADUserLicense -ObjectId $userToOffboard -AssignedLicenses $body
+  }
+   
+  write-host "Removed licenses:"
+  $AssignedLicensesTable
+  write-host "Disabling User sign in" -ForegroundColor Green
+  Set-AzureADUser -ObjectID $userToOffboard -AccountEnabled $false
+  Remove-PSSession $session
   $RequestText = @"
-$MFARequest
+$userToOffboard
 "@
 }
 else {
   $RequestText = @"
   <form action="create" method="POST">
-    <label for="Username">Username</label><br>
-    <input type="text" name="username" id="username" value="user@user.com"><br>
-    Use the Create button below to generate a MFA push towards the user.<br>
-    <input class="button" name="Submit" type="submit" value="Create">
+    <label for="userToOffboard">Username</label><br>
+    <input type="text" name="username" id="userToOffboard" value="user@user.com"><br>
+    Use the 'Offboard' button below to start offboarding the user right away.<br>
+    <input class="button" name="Submit" type="submit" value="Offboard">
   </form>
 "@
 }
@@ -78,8 +140,8 @@ div {
 <body>
 <center>
 $($Logo)
-<title>Identity Confirmation Portal</Title>
-<h3>Confirm an identity by sending a MFA request.</h3>
+<title>Office 365 Offboard User Portal</Title>
+<h3>Automatically Offboard a Microsoft 365/Azure AD User and archive their account.</h3>
 <div>
 $RequestText
 </div>
